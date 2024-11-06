3918b1fa2da2e6907deb32536192fab890e5c4fd
janine pamintuan <jaja.ninnin@gmail.com>
2024-11-06 14:38:14 -0500

    react router labbb

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from "react";
+import NavBar from '../components/NavBar';
 
 function Actors() {
+  const [ actors, setActors ] = useState([])
+  
+  useEffect(() =>{
+    fetch(`http://localhost:4000/actors`)
+    .then(r => r.json())
+    .then(data => {
+      setActors(data);
+  })
+    .catch(error => console.error(error));
+  }, []);
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actors.map((actor) => {
+          return <article key={actor.id}>
+            <h2>{actor.name}</h2>
+              <ul>{actor.movies.map((movie) => {
+                return <li key={movie}>{movie}</li>
+              })}</ul>
+        </article>})}
       </main>
     </>
   );
